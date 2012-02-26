5f710c4f6bbcbd45ea0ed46c13cf6ab89429f7ec
Danny Thomas <dmthomas@gmail.com>
2012-02-26 23:19:31 +0000

    OSQA-815: More minification improvements
    * All js files use non-minified versions in the media tag (where available)
    * Move viewbox.css to styles directory
    * Fix problem with compress.sh that would cause minified resources to be compressed

--- forum/skins/default/media/compress.sh
+++ forum/skins/default/media/compress.sh
@@ -2,11 +2,11 @@
 
 # Compress all js and style files, replacing existing minified versions
 
-for i in `find js/ style/ -type f -name '*.css' -or -name '*.js' -not -name '*.min.css' -not -name '*.min.js'`
+for i in `find js/ style/ -type f \( -name '*.js' -o -name '*.css' \) ! -name '*.min.*'`
 do
   # From http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
   filename="${i##*/}"                      # Strip longest match of */ from start
-  dir="${i:0:${#i} - ${#filename}}" # Substring from 0 thru pos of filename
+  dir="${i:0:${#i} - ${#filename}}"        # Substring from 0 thru pos of filename
   base="${i%.[^.]*}"                       # Strip shortest match of . plus at least one non-dot char from end
   ext="${i:${#base} + 1}"                  # Substring from len of base thru end
 
