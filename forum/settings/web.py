f7b4d807aa0d3c193dde53ad2b2a75737cdd7670
Danny Thomas <dmthomas@gmail.com>
2012-02-29 16:27:25 +0000

    SQA-817: Frontend performance improvements
    * Don't use the Google Library API loader, reference resources directly. CDN should be the default
    * Remove jquery-ui default style import from style.css and link resource directly
    * There isn't a minified viewbox.js, change back again
    * Add configurable CSS sprite support
    * Automate sprite generation with SmartSprites tool
    * Combine jQuery and jQuery UI files when served locally

--- forum/settings/web.py
+++ forum/settings/web.py
@@ -8,12 +8,17 @@ from django.forms.widgets import Textarea
 
 WEB_SET = SettingSet('web', _('Web settings'), _("Web settings for the application"))
 
-CDN_ON = Setting('CDN_ON', False, WEB_SET, dict(
+CDN_ON = Setting('CDN_ON', True, WEB_SET, dict(
 label = _("Use content distribution network"),
-help_text = _("Check if you want to use the Google Libraries API and CDN locations to load JavaScript libraries."),
+help_text = _("Check if you want to use the Google Libraries API and CDN locations to load JavaScript libraries. Google hosted CSS files are not minified and jQuery resources take one extra request, however this is offset by location and serving key resource files from a different hostname."),
 required=False))
 
 MINIFIED_RESOURCES = Setting('MINIFIED_RESOURCES', True, WEB_SET, dict(
 label = _("Use minified resources"),
 help_text = _("Check if you want to use use minified JavaScript and CSS resources when available. Non-minified resources are always used if DEBUG is enabled."),
+required=False))
+
+CSS_SPRITES = Setting('CSS_SPRITES', True, WEB_SET, dict(
+label = _("Use CSS sprites"),
+help_text = _("Check if you want to use use CSS sprites instead of individual images."),
 required=False))
--- forum/skins/default/media/compress.sh
+++ forum/skins/default/media/compress.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 # Compress all js and style files, replacing existing minified versions
+# Must be executed from the media directory
 
+wd=$PWD
+
+echo "Generating CSS sprite image..."
+cd tools/smartsprites-0.2.8
+./smartsprites.sh --css-files $wd/style/style.css
+cd $wd
+echo
+
+echo "Compressing JavaScript and CSS resources..."
 for i in `find js/ style/ -type f \( -name '*.js' -o -name '*.css' \) ! -name '*.min.*'`
 do
   # From http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
@@ -12,5 +22,5 @@ do
 
   filename=${base}.min.${ext}
   echo "Compressing $i to $filename"
-  java -jar yuicompressor-2.4.7.jar -o $filename $i
+  java -jar tools/yuicompressor-2.4.7.jar -o $filename $i
 done
